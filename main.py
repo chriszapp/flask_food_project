58bec140c2d9e6c27d61ba3c44e02f931d56c475
chris_z <cristina.zappullo@gmail.com>
2022-01-26 21:44:44 +0000

    add local run code

--- main.py
+++ main.py
@@ -41,7 +41,11 @@ def search_page():
         return render_template('result.html', obj = id_input, data = filtering_df(id_input, products))
 
 if __name__ == "__main__":  # Makes sure this is the main process
-	app.run( # Starts the site
+	# For Repl.it
+    app.run( # Starts the site
 		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
 		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
 	)
+
+    # # For Local
+    # app.run()
